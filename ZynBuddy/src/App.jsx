5009164fc1c3cdc58a0f0a58107a10160b4fceac
Larkin Campbell <Larkincampbell24@gmail.com>
2024-04-27 20:47:06 -0700

    renaming files

---
+++ ZynBuddy/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function ZynTimer() {
+  
+
+
+
+  return (
+    <div className='title-header'>
+      <div className='title-text'>ZynBuddy</div>
+      <div className='title-buttons'>
+        <div id='log-status'>Log in</div>
+        <div id='profile'>Profile</div>
+      </div>
+    </div>
+  )
+}
+
+export default ZynTimer
